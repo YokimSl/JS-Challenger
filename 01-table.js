8b7bf44b30c87a84d6410a599b7e981cd5f75cdd
YokimSl <jhonnata1337@gmail.com>
2023-12-28 20:46:57 -0300

    fisrt commit

---
+++ 01-table.js
@@ -0,0 +1,33 @@
+// Faça uma função chamada table que tem como parâmetro de entrada um número 
+// e devolve a tabuada desse mesmo número do valor 1 a 10. 
+// Um exemplo de saída para a tabuada do 6 seria:
+
+// "The table of 6 is:"
+// "1 x 6 = 6";
+// "2 x 6 = 12";
+// "3 x 6 = 18";
+// "4 x 6 = 24";
+// "5 x 6 = 30";
+// "6 x 6 = 36";
+// "7 x 6 = 42";
+// "8 x 6 = 48";
+// "9 x 6 = 54";
+// "10 x 6 = 60";
+
+
+
+
+function table(number) {
+    for(let i = 1; i <= 10; i++){
+        console.log(`${number} x ${i} = ${number * i}`);
+
+    }
+}
+
+table(7);
+
+// Bônus:
+// Agora coloque mais um parâmetro na função: ao invés da tabuada ir de 1 a 10, 
+// agora ela pode ir de 1 até x.
+
+
